2898939cb7963e06544672b2c1cb56b8a9b844d2
hansenjonatann <hansenjonatann@gmail.com>
2024-04-29 01:59:42 +0700

    feat: make footer component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,15 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-import { Main } from "next/document";
-import { Inter } from "next/font/google";
 import Head from "next/head";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>HJ Codin Community</title>
+        <title>Komunitas HJ Codin Tech</title>
       </Head>
       <Navbar />
+      <Footer />
     </>
   );
 }
