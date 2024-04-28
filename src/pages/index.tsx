c8f0c86bc1bf503dad080a0d31595c776def7edc
hansenjonatann <hansenjonatann@gmail.com>
2024-04-29 01:39:31 +0700

    fix: fixing error importing <Head>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,11 @@
 import Navbar from "@/components/Navbar";
-import { Main } from "next/document";
-import { Inter } from "next/font/google";
 import Head from "next/head";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>HJ Codin Community</title>
+        <title>Komunitas HJ Codin Tech</title>
       </Head>
       <Navbar />
     </>
